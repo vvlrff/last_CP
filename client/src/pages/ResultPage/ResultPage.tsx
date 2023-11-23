d11cd9e384c7234e03b5c70cc832132d20f9619c
wlrff <vvlrff@gmail.com>
2023-11-23 20:56:49 +0300

    /

--- client/src/pages/ResultPage/ResultPage.tsx
+++ client/src/pages/ResultPage/ResultPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import s from "./ResultPage.module.scss";
+// import s from "./ResultPage.module.scss";
 
 
 const ResultPage: React.FC = () => {
