2b92bb9a921e77bdc749ae004e75e7720b62854a
LegionaryByte <148693888+LegionaryByte@users.noreply.github.com>
2023-11-24 12:55:54 +0300

    [interface]

--- backend/src/data_IO/interface.py
+++ backend/src/data_IO/interface.py
@@ -1,19 +1,20 @@
-from bert_classifier import BertClassifier
+import os
+from .models.bert_classifier import BertClassifier
 import pandas as pd
-
+from .schemas import ResponseSchema
 
 class Predictor:
     def __init__(self) -> None:
         
-        self.topic_group_classifier = BertClassifier(model_path=r'C:\Users\Chubu\OneDrive\Рабочий стол\last_CP\NLP\NaturaLP_LaBSE_topic_group.pt',
+        self.topic_group_classifier = BertClassifier(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'NaturaLP_LaBSE_topic_group.pt'),
                                                     tokenizer_path='cointegrated/LaBSE-en-ru')
         
-        self.topic_classifier = BertClassifier(model_path=r'C:\Users\Chubu\OneDrive\Рабочий стол\last_CP\NLP\NaturaLP_LaBSE_topic.pt',
-                                                tokenizer_path='cointegrated/LaBSE-en-ru')
+        self.topic_classifier = BertClassifier(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'NaturaLP_LaBSE_topic.pt'),
+                                                    tokenizer_path='cointegrated/LaBSE-en-ru')
         
         # тут будет еще одна модель
-        
-        data = pd.read_csv("train_dataset_train.csv", delimiter=";")
+
+        data = pd.read_csv(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'train_dataset_train.csv'), delimiter=";")
         data = data.rename(columns={'Исполнитель':'executor', 'Группа тем': 'topic_group', 'Текст инцидента':'text', 'Тема': 'topic'})
 
         self.topic_group_collector = dict(enumerate(list(data.topic_group.unique())))
@@ -22,14 +23,14 @@ class Predictor:
 
     def predict(self, text: str):
 
-        topic_group = self.topic_group_classifier.predict(text)
-        topic = self.topic_classifier.predict(text)
+        topic_group = self.topic_group_collector[self.topic_group_classifier.predict(text)]
+        topic = self.topic_collector[self.topic_classifier.predict(text)]
         # тут будет определение исполнителя
-
-        return self.topic_group_collector[topic_group], self.topic_collector[topic] # тут будет еще исполнитель
+        
+        return ResponseSchema(executor='Исполнитель А', topic_group=str(topic_group), text_incident=text, topic=str(topic)) # тут будет еще исполнитель
     
 
-if __name__ == '__main__':
-    tester = Predictor()
-    print(tester.predict('Здравствуйте, скажите пожалуйста сколько времени занимает подпись документов на операцию в г Лысьва?В понедельник были переданы документы на подпись на операцию в г Перми сегодня четверг документы до сих пор не пришли. Операция была назначена на 13 сентября.Из Москвы посылка пришла быстрее.'))
+# if __name__ != '__main__':
+#     tester = Predictor()
+#     print(tester.predict('Здравствуйте, скажите пожалуйста сколько времени занимает подпись документов на операцию в г Лысьва?В понедельник были переданы документы на подпись на операцию в г Перми сегодня четверг документы до сих пор не пришли. Операция была назначена на 13 сентября.Из Москвы посылка пришла быстрее.'))
 
