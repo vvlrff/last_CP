a6a65c2475bcd4472c203a794fe178fec3c8c860
LegionaryByte <148693888+LegionaryByte@users.noreply.github.com>
2023-11-24 14:45:01 +0300

    [executor add]

--- backend/src/data_IO/interface.py
+++ backend/src/data_IO/interface.py
@@ -14,6 +14,8 @@ class Predictor:
         self.topic_classifier = BertClassifier(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'NaturaLP_LaBSE_topic.pt'),
                                                     tokenizer_path='cointegrated/LaBSE-en-ru')
         
+        self.executor_classifier = BertClassifier(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'NaturaLP_LaBSE_executor.pt'),
+                                                tokenizer_path='cointegrated/LaBSE-en-ru')
         # тут будет еще одна модель
 
         data = pd.read_csv(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'train_dataset_train.csv'), delimiter=";")
@@ -21,6 +23,8 @@ class Predictor:
 
         self.topic_group_collector = dict(enumerate(list(data.topic_group.unique())))
         self.topic_collector = dict(enumerate(list(data.topic.unique())))
+        self.executor_collector = dict(enumerate(list(data.executor.unique())))
+
         # тут будет еще однин словарь
         self.addr_extractor = AddrExtractor(MorphVocab())
 
@@ -38,12 +42,12 @@ class Predictor:
         
     def predict(self, text: str):
 
+        executor = self.executor_collector[self.executor_classifier.predict(text)]
         topic_group = self.topic_group_collector[self.topic_group_classifier.predict(text)]
         topic = self.topic_collector[self.topic_classifier.predict(text)]
-        # тут будет определение исполнителя
         adress = self.find_adress(text)
-        print(adress)
-        return ResponseSchema(executor='Исполнитель А', topic_group=str(topic_group), text_incident=text, topic=str(topic), adress=adress) # тут будет еще исполнитель
+
+        return ResponseSchema(executor=executor, topic_group=str(topic_group), text_incident=text, topic=str(topic), adress=adress) # тут будет еще исполнитель
     
 
 # if __name__ != '__main__':
