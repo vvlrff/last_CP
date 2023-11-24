a6a2a93d0ffd2ff7b0d9f95329e38d8142548691
wlrff <vvlrff@gmail.com>
2023-11-24 17:57:05 +0300

    changed map

--- client/src/components/Map/RussiaMap.tsx
+++ client/src/components/Map/RussiaMap.tsx
@@ -1,29 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import L, { LatLngExpression } from "leaflet";
-import { RussiaRailwayMapProps, TrainData, stationTrainData } from "../../models/MapInterfaces";
+// import { RussiaRailwayMapProps, TrainData, stationTrainData } from "../../models/MapInterfaces";
 import blackStation from "../../assets/images/blackStation.png";
 import greenStation from "../../assets/images/greenStation.png";
 import "leaflet/dist/leaflet.css";
 import { IData } from "../../pages/MapPage/MapPage";
 
 
-const RussiaMap = ({data}: {data: IData}) => {
+const RussiaMap = ({ data }: { data: IData }) => {
 
     console.log(data)
 
-    // const greenStationIcon = new L.Icon({
-    //     iconUrl: greenStation, // Replace with your green icon path
-    //     iconSize: [30, 30],
-    //     iconAnchor: [15, 30],
-    //     popupAnchor: [0, -30],
-    // });
 
-    // const greyStationIcon = new L.Icon({
-    //     iconUrl: blackStation, // Replace with your grey icon path
-    //     iconSize: [30, 30],
-    //     iconAnchor: [15, 30],
-    //     popupAnchor: [0, -30],
-    // });
 
     const mapStyle = {
         height: "calc(93vh)",
@@ -47,8 +35,36 @@ const RussiaMap = ({data}: {data: IData}) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
 
+            {data.latitude && data.longitude && (
+                <Marker position={[data.latitude, data.longitude]}>
+                    <Popup>
+                        <div>
+                            <h3>{data.executor}</h3>
+                            <p>Текст инцидента: {data.text_incident}</p>
+                            <p>Тема: {data.topic}</p>
+                            <p>Город: {data.adress?.город}</p>
+                        </div>
+                    </Popup>
+                </Marker>
+            )}
+
         </MapContainer>
     );
 };
 
 export default RussiaMap;
+
+
+// const greenStationIcon = new L.Icon({
+//     iconUrl: greenStation, // Replace with your green icon path
+//     iconSize: [30, 30],
+//     iconAnchor: [15, 30],
+//     popupAnchor: [0, -30],
+// });
+
+// const greyStationIcon = new L.Icon({
+//     iconUrl: blackStation, // Replace with your grey icon path
+//     iconSize: [30, 30],
+//     iconAnchor: [15, 30],
+//     popupAnchor: [0, -30],
+// });
--- client/src/models/MapInterfaces.ts
+++ client/src/models/MapInterfaces.ts
@@ -1,4 +1,4 @@
-export interface RussiaRailwayMapProps {
+export interface RussiaMapProps {
     data?: TrainData[];
 }
 
