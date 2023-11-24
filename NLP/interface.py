db75c009e2620cbae09be419ef9b9193b21d9f39
Chief-tain <125179070+Chief-tain@users.noreply.github.com>
2023-11-24 18:45:32 +0300

    sentiment

--- NLP/interface.py
+++ NLP/interface.py
@@ -1,7 +1,8 @@
 from bert_classifier import BertClassifier
 import pandas as pd
-
 from natasha import (MorphVocab, LOC, AddrExtractor)
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 class Predictor:
     def __init__(self) -> None:
@@ -21,9 +22,26 @@ class Predictor:
         self.topic_group_collector = dict(enumerate(list(data.topic_group.unique())))
         self.topic_collector = dict(enumerate(list(data.topic.unique())))
         self.executor_collector = dict(enumerate(list(data.executor.unique())))
+        self.setminent_collector = {'negative': 'Негативная', 'neutral': 'Нейтральная','positive': 'Позитивная'}
 
         self.addr_extractor = AddrExtractor(MorphVocab())
 
+        self.sentimen_tokenizer = AutoTokenizer.from_pretrained('cointegrated/rubert-tiny-sentiment-balanced')
+        self.sentimen_model = AutoModelForSequenceClassification.from_pretrained('cointegrated/rubert-tiny-sentiment-balanced')
+
+        print('initialized')
+
+    def get_sentiment(self, text, return_type='label'):
+        """ Calculate sentiment of a text. `return_type` can be 'label', 'score' or 'proba' """
+        with torch.no_grad():
+            inputs = self.sentimen_tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(self.sentimen_model.device)
+            proba = torch.sigmoid(self.sentimen_model(**inputs).logits).cpu().numpy()[0]
+        if return_type == 'label':
+            return self.sentimen_model.config.id2label[proba.argmax()]
+        elif return_type == 'score':
+            return proba.dot([-1, 0, 1])
+        return proba
+    
     def find_adress(self, text: str):
         try:
             matches = self.addr_extractor(text)
@@ -41,6 +59,7 @@ class Predictor:
         final_answer = {'Группа тем': self.topic_group_collector[self.topic_group_classifier.predict(text)],
                         'Тема': self.topic_collector[self.topic_classifier.predict(text)],
                         'Исполнитель': self.executor_collector[self.executor_classifier.predict(text)],
+                        'Тональность': self.setminent_collector[self.get_sentiment(text)],
                         'Адрес': self.find_adress(text)}
         
         return final_answer
--- backend/src/data_IO/interface.py
+++ backend/src/data_IO/interface.py
@@ -1,6 +1,8 @@
 import os
 from natasha import (MorphVocab, LOC, AddrExtractor)
 import pandas as pd
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 from .models.bert_classifier import BertClassifier
 from .schemas import ResponseSchema
@@ -16,7 +18,6 @@ class Predictor:
         
         self.executor_classifier = BertClassifier(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'NaturaLP_LaBSE_executor.pt'),
                                                 tokenizer_path='cointegrated/LaBSE-en-ru')
-        # тут будет еще одна модель
 
         data = pd.read_csv(os.path.join(os.path.join(os.getcwd(), 'src', 'data_IO', 'models'), 'train_dataset_train.csv'), delimiter=";")
         data = data.rename(columns={'Исполнитель':'executor', 'Группа тем': 'topic_group', 'Текст инцидента':'text', 'Тема': 'topic'})
@@ -24,9 +25,12 @@ class Predictor:
         self.topic_group_collector = dict(enumerate(list(data.topic_group.unique())))
         self.topic_collector = dict(enumerate(list(data.topic.unique())))
         self.executor_collector = dict(enumerate(list(data.executor.unique())))
+        self.setminent_collector = {'negative': 'Негативная', 'neutral': 'Нейтральная','positive': 'Позитивная'}
 
-        # тут будет еще однин словарь
         self.addr_extractor = AddrExtractor(MorphVocab())
+
+        self.sentimen_tokenizer = AutoTokenizer.from_pretrained('cointegrated/rubert-tiny-sentiment-balanced')
+        self.sentimen_model = AutoModelForSequenceClassification.from_pretrained('cointegrated/rubert-tiny-sentiment-balanced')
 
     def find_adress(self, text: str):
         try:
@@ -40,17 +44,34 @@ class Predictor:
         except Exception:
             return adress_collector
         
+    def get_sentiment(self, text, return_type='label'):
+        """ Calculate sentiment of a text. `return_type` can be 'label', 'score' or 'proba' """
+        with torch.no_grad():
+            inputs = self.sentimen_tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(self.sentimen_model.device)
+            proba = torch.sigmoid(self.sentimen_model(**inputs).logits).cpu().numpy()[0]
+        if return_type == 'label':
+            return self.sentimen_model.config.id2label[proba.argmax()]
+        elif return_type == 'score':
+            return proba.dot([-1, 0, 1])
+        return proba
+        
     def predict(self, text: str):
 
         executor = self.executor_collector[self.executor_classifier.predict(text)]
         topic_group = self.topic_group_collector[self.topic_group_classifier.predict(text)]
         topic = self.topic_collector[self.topic_classifier.predict(text)]
+        sentiment = self.setminent_collector[self.get_sentiment(text)]
         adress = self.find_adress(text)
         latitude, longitude = main(adress)
-        print(latitude, longitude)
-        return ResponseSchema(executor=executor, topic_group=topic_group, 
-                              text_incident=text, topic=topic, adress=adress,
-                              latitude=latitude,longitude=longitude )
+
+        return ResponseSchema(executor=executor,
+                              topic_group=topic_group, 
+                              text_incident=text,
+                              topic=topic,
+                              adress=adress,
+                              sentiment=sentiment,
+                              latitude=latitude,
+                              longitude=longitude)
     
 
 
