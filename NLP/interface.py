96179942d778bc4af3a80fe0d2d1cb97d232f3f4
Chief-tain <125179070+Chief-tain@users.noreply.github.com>
2023-11-24 14:08:15 +0300

    new interface

--- NLP/interface.py
+++ NLP/interface.py
@@ -1,6 +1,7 @@
 from bert_classifier import BertClassifier
 import pandas as pd
 
+from natasha import (MorphVocab, LOC, AddrExtractor)
 
 class Predictor:
     def __init__(self) -> None:
@@ -20,16 +21,36 @@ class Predictor:
         self.topic_collector = dict(enumerate(list(data.topic.unique())))
         # тут будет еще однин словарь
 
+        self.addr_extractor = AddrExtractor(MorphVocab())
+
+    def find_adress(self, text: str):
+        try:
+            matches = self.addr_extractor(text)
+            facts = [i.fact.as_json for i in matches]
+            adress_collector = dict()
+            for i in range(len(facts)):
+                tmp = list(facts[i].values())
+                adress_collector[tmp[1]] = tmp[0]
+            return adress_collector
+        except Exception:
+            return ''
+
     def predict(self, text: str):
 
         topic_group = self.topic_group_classifier.predict(text)
         topic = self.topic_classifier.predict(text)
         # тут будет определение исполнителя
+        adress = self.find_adress(text)
 
-        return self.topic_group_collector[topic_group], self.topic_collector[topic] # тут будет еще исполнитель
+        final_answer = {'Группа тем': self.topic_group_collector[topic_group],
+                        'Тема': self.topic_collector[topic],
+                        'Исполнитель': '',
+                        'Адрес': adress}
+        
+        return final_answer
     
 
 if __name__ == '__main__':
     tester = Predictor()
-    print(tester.predict('Здравствуйте, скажите пожалуйста сколько времени занимает подпись документов на операцию в г Лысьва?В понедельник были переданы документы на подпись на операцию в г Перми сегодня четверг документы до сих пор не пришли. Операция была назначена на 13 сентября.Из Москвы посылка пришла быстрее.'))
+    print(tester.predict('Добрый день. Сегодня, 20.08.22, моя мать шла по улице Ленина между домами 96 и 94. Фонари не горят, упала в яму, которую не видно. Сильно ударилась, остались синяки, очень больно. Благо шла не одна.<br>Уважаемая Администрация, сделайте с этим что-нибудь, да и не только с этим. Ходить опасно не только взрослым, но и детям. Если бы упал маленький ребёнок, было бы намного хуже. Фото прилагаю. Спасибо!'))
 
